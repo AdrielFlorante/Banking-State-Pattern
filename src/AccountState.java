5f61a896878ad39dc1fab3e38174436510cfe708
AdrielFlorante <adriel.florante@neu.edu.ph>
2025-02-23 20:10:38 +0800

    Changed root of repo

---
+++ src/AccountState.java
@@ -0,0 +1,7 @@
+public interface AccountState {
+    public void deposit(double amount);
+    public void withdraw(double amount);
+    public void activate();
+    public void suspend();
+    public void close();
+}
